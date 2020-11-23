625320b076b0c858231788e94908691cafa747d7
thanhnew2000 <giacmonghoanmyy@gmail.com>
2020-11-23 09:59:17 +0700

    commit link api

--- android/app/src/api/axiosApi.js
+++ android/app/src/api/axiosApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosAPItoken = (token) => {
     return axios.create({
-        baseURL: "http://10.24.11.88:8080/api",
+        baseURL: "http://34.122.241.19:8080/api",
         headers: {
             "Content-type": "application/json",
             'Authorization': `Bearer ${token}`
--- android/app/src/api/ipApi.js
+++ android/app/src/api/ipApi.js
@@ -1,2 +1,2 @@
 
-export default'http://10.24.11.88:8080/'
+export default'http://34.122.241.19:8080/'
